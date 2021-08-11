87c66757fa18bc21b272f43642a39537fd13773e
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2021-08-11 22:31:22 +0530

    file_handling: Update functions
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- src/file_handling/functions.rs
+++ src/file_handling/functions.rs
@@ -19,33 +19,33 @@ pub fn get_logger_from_json<P: AsRef<Path>>(path: P) -> Result<Logger, Box<dyn e
     Ok(logger)
 }
 
-pub fn open_file(name: String) -> std::fs::File {
+pub fn open_file(name: &str) -> std::fs::File {
     let file = OpenOptions::new()
         .write(true)
         .append(true)
-        .open(name.as_str())
+        .open(name)
         .unwrap();
     file
 }
 
-pub fn create_file(name: String) -> std::fs::File {
+pub fn create_file(name: &str) -> std::fs::File {
     let file = std::fs::OpenOptions::new()
         .write(true)
         .create(true)
         .read(true)
-        .open(name.as_str())
+        .open(name)
         .expect("File could not be created.");
     file
 }
 
-pub fn delete_file(name: String) {
-    if Path::new(name.as_str()).exists() {
-        std::fs::remove_file(name.as_str())
+pub fn delete_file(name: &str) {
+    if Path::new(name).exists() {
+        std::fs::remove_file(name)
             .expect("File could not be deleted.");
     }
 }
 
-pub fn write_to_file(file_name: String, entry: &str) {
+pub fn write_to_file(file_name: &str, entry: &str) {
     let mut file = open_file(file_name);
     writeln!(file, "{}", entry)
         .expect("File could not be written into.");
